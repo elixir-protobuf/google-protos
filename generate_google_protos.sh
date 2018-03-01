1264346d4e3bdb9374f18e9b65712ea57a978852
Bing Han <h.bing612@gmail.com>
2018-03-01 11:06:55 +0800

    remove protos we may not need

--- generate_google_protos.sh
+++ generate_google_protos.sh
@@ -2,14 +2,10 @@
 
 PROTOS=("
     protobuf/src/google/protobuf/any.proto
-    protobuf/src/google/protobuf/api.proto
     protobuf/src/google/protobuf/duration.proto
     protobuf/src/google/protobuf/empty.proto
-    protobuf/src/google/protobuf/field_mask.proto
-    protobuf/src/google/protobuf/source_context.proto
     protobuf/src/google/protobuf/struct.proto
     protobuf/src/google/protobuf/timestamp.proto
-    protobuf/src/google/protobuf/type.proto
     protobuf/src/google/protobuf/wrappers.proto
 ")
 
