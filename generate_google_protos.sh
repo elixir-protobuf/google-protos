aa7febc5ed56bea805fbceaef1002575c550cd72
Xiangrong Hao <falood@gmail.com>
2018-03-01 11:12:50 +0800

    first commit (#1)
    * first commit
    
    * rename google_protobuf to google_protos
    
    * remove unused files
    
    * remove protos we may not need
    
    * regenrate protos

---
+++ generate_google_protos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROTOS=("
+    protobuf/src/google/protobuf/any.proto
+    protobuf/src/google/protobuf/duration.proto
+    protobuf/src/google/protobuf/empty.proto
+    protobuf/src/google/protobuf/struct.proto
+    protobuf/src/google/protobuf/timestamp.proto
+    protobuf/src/google/protobuf/wrappers.proto
+")
+
+rm -rf ./lib/google_protos/*
+
+for file in $PROTOS; do
+    protoc -I ./protobuf/src/google/protobuf/ --elixir_out=plugins=grpc:./lib/google_protos $file
+done
---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule GoogleProtos.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :google_protos,
+      name: "Google Protos",
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      description: "Protos by Google",
+      package: package()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {GoogleProtos, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:protobuf, "~> 0.5"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Tony Han"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/tony612/google-protos"},
+      files: ~w(mix.exs README.md lib config LICENSE)
+    ]
+  end
+end
