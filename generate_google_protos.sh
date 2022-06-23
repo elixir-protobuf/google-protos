f8c52439c6cadaf9d24b46da422e8c483d229fef
Yordis Prieto <yordis.prieto@gmail.com>
2022-06-23 04:49:32 -0400

    Add FieldMask proto (#13)
    Co-authored-by: Andrew Shaw <ashaw@drwuk.com>

--- generate_google_protos.sh
+++ generate_google_protos.sh
@@ -4,6 +4,7 @@ PROTOS=("
     protobuf/src/google/protobuf/any.proto
     protobuf/src/google/protobuf/duration.proto
     protobuf/src/google/protobuf/empty.proto
+    protobuf/src/google/protobuf/field_mask.proto
     protobuf/src/google/protobuf/struct.proto
     protobuf/src/google/protobuf/timestamp.proto
     protobuf/src/google/protobuf/wrappers.proto
@@ -12,5 +13,5 @@ PROTOS=("
 rm -rf ./lib/google_protos/*
 
 for file in $PROTOS; do
-    protoc -I ./protobuf/src/google/protobuf/ --elixir_out=plugins=grpc:./lib/google_protos $file
+  protoc -I ./protobuf/src/google/protobuf/ --elixir_out=plugins=grpc:./lib/google_protos $file
 done
