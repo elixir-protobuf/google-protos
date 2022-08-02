c873bca5acc6ae9ea6747222d33ac6fcdc2f61c4
Andrea leopardi <an.leopardi@gmail.com>
2022-08-02 17:21:27 +0200

    Fix requirement in mix.exs

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule GoogleProtos.MixProject do
       app: :google_protos,
       name: "Google Protos",
       version: "0.3.0",
-      elixir: "~> 1.",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Protos by Google",
