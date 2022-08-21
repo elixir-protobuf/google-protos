fde89eb2adf8fa54b3c0f99c51636d0589cf146e
Yordis Prieto <yordis.prieto@gmail.com>
2022-08-21 02:52:38 -0400

    Add "how-to" section to the README (#16)

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+# Google Protos
+
 For Elixir files generated from [Google's protobuf files](https://github.com/google/protobuf/tree/master/src/google/protobuf) using [protobuf-elixir](https://github.com/tony612/protobuf-elixir).
+
+## How-To
+
+### Convert `Google.Protobuf.Timestamp` to `DateTime`
+
+1. Convert timestamp seconds to nanoseconds.
+2. Add the timestamp nanos.
+3. Convert to `DateTime` using `DateTime.from_unix!/2`.
+
+```elixir
+timestamp = Google.Protobuf.Timestamp.new(seconds: 5, nanos: 100)
+DateTime.from_unix!(timestamp.seconds * 1_000_000_000 + timestamp.nanos, :nanosecond)
+```
--- lib/google_protos/struct.pb.ex
+++ lib/google_protos/struct.pb.ex
@@ -4,6 +4,7 @@ defmodule Google.Protobuf.NullValue do
 
   field :NULL_VALUE, 0
 end
+
 defmodule Google.Protobuf.Struct.FieldsEntry do
   @moduledoc false
   use Protobuf, map: true, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
@@ -11,12 +12,14 @@ defmodule Google.Protobuf.Struct.FieldsEntry do
   field :key, 1, type: :string
   field :value, 2, type: Google.Protobuf.Value
 end
+
 defmodule Google.Protobuf.Struct do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
 
   field :fields, 1, repeated: true, type: Google.Protobuf.Struct.FieldsEntry, map: true
 end
+
 defmodule Google.Protobuf.Value do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
@@ -35,6 +38,7 @@ defmodule Google.Protobuf.Value do
   field :struct_value, 5, type: Google.Protobuf.Struct, json_name: "structValue", oneof: 0
   field :list_value, 6, type: Google.Protobuf.ListValue, json_name: "listValue", oneof: 0
 end
+
 defmodule Google.Protobuf.ListValue do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
