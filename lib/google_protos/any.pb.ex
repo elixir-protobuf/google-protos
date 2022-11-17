f6e5e5dcadd8f5348dc74f8a5804dbeb76a075af
Jan Trefil <jan.trefil@divvypay.com>
2022-11-17 10:30:37 +0100

    Upgrade protobuf to 0.11

--- lib/google_protos/any.pb.ex
+++ lib/google_protos/any.pb.ex
@@ -1,6 +1,6 @@
 defmodule Google.Protobuf.Any do
   @moduledoc false
-  use Protobuf, protoc_gen_elixir_version: "0.10.0", syntax: :proto3
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
   field :type_url, 1, type: :string, json_name: "typeUrl"
   field :value, 2, type: :bytes
